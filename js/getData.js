d1ed677e87914098397af3cc8c55e27386c0fd03
gianca243 <gianca243@gmail.com>
2020-09-29 11:34:25 -0500

    first commit

---
+++ js/getData.js
@@ -0,0 +1,32 @@
+const API = 'https://rickandmortyapi.com/api/character'
+
+
+const getData = (api)=>{
+  return fetch(api)
+    .then( (response) =>response.json())
+    .then((json)=>{
+      // console.log('json-->', json);
+      llenarDatos(json)
+    })  
+    .catch( (error) => {
+      console.log('error', error);
+    })
+}
+const llenarDatos = (data)=>{
+  let html = ""
+  data.results.forEach((pj) => {
+      html += '<div class="col">'
+        html += '<div class="card" style="width: 10rem;">'
+        html += `<img src="${pj.image}" class="card-img-top" alt="...">`
+          html += '<div class="card-body" >'
+          html += ` <h5 class="card-title">${pj.name}</h5>`
+          html += `<p class="card-text">Status: ${pj.status}</p>`
+          html += `<p class="card-text">Especie: ${pj.species}</p>`
+          html += `<p class="card-text">Genero: ${pj.gender}</p>`
+          html += '</div>'
+        html += '</div>'
+      html += '</div>'
+  });
+  document.getElementById("datosPersonaje").innerHTML= html
+}
+getData(API)
